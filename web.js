1771aff194457a435c703df87265f23bc402a29a
tdebruille <tdebruille@myopera.com>
2013-07-11 14:01:49 +0200

    Modified web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var out = fs.readFile('index.html');
+	var out = fs.readFileSync('index.html');
 	var out = out.toString();
   response.send(out);
 });
